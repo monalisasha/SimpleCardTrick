9f62bd40db864e6e27c3abd8a376669453ce6134
monalisasha <monalisa0410@gmail.com>
2019-10-07 13:40:59 -0700

    changing the function

--- card.py
+++ card.py
@@ -1,29 +1,29 @@
+# Created by Monalisa Sha on 09-07-2019
+
 import random
 from random import shuffle
 
-
-
-values = ['2','3','4','5','6','7','8','9','10','Jack','Queen','King','Ace']
+# Deck of 52 cards
+values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
 suites = ['Hearts', 'Clubs', 'Diamonds', 'Spades']
-deck = [[v + ' of ' + s,v] for s in suites for v in values]
-print(deck)
+deck = [v for s in suites for v in values]
 
-
+#For drawing random 10 cards
 def draw_card(n, deck):
     random.shuffle(deck)
     cards_selected = [deck.pop() for k in range(n)]
+    print(cards_selected)
+    for i in range(0, 5):
+        cards_selected += [cards_selected.pop(0)]
+    print(cards_selected)
 
-#     for i in range(n):
-#         cards_selected += [cards_selected.pop(0)]
-#
-# left = cards_selected[:5]
-#
-# right = cards_selected[5:]
-# new_deck = right + left
-#
-#
-# for i in range(0,6):
-#
-#        cards_selected += [cards_selected.pop(0)]
-#
-# print (cards_selected[3])
+    left = cards_selected[:5]
+    right = cards_selected[5:]
+
+    new_deck = right + left
+    print(new_deck)
+
+    for i in range(0, 6):
+        new_deck += [new_deck.pop(0)]
+
+    print([new_deck[3]])
